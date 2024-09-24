0eac9ca024beddfdc180ae2f059eb5337df1a932
ValiValista <a01647566@tec.mx>
2024-09-24 10:51:17 -0600

    Added a basic program flow sketch. This is a guideline and can be changed to fit the needs and our available resources.

--- main/main.py
+++ main/main.py
@@ -1,14 +1,28 @@
 # todo = to do list, this shows up in github and IDEs as a todo list
-# How to use github https://docs.github.com/en/get-started/start-your-journey/hello-world
-## 1. Create a new branch
-## 2. Make changes to the branch
-## 3. Commit changes
-## 4. Push changes to the branch
-## 5. Create a pull request
-## 6. Review changes
-## 7. Merge changes
+# How to use GitHub documentation https://docs.github.com/en/get-started/start-your-journey/hello-world
+## Github on VSCODe https://code.visualstudio.com/docs/editor/github
+## Github on PyCharm https://www.jetbrains.com/help/pycharm/github.html
+    #How to use GitHub
+    ## 1. Create a new branch
+    ## 2. Make changes to the branch
+    ## 3. Commit changes
+    ## 4. Push changes to the branch
+    ## 5. Create a pull request
+    ## 6. Review changes
+    ## 7. Merge changes
 
 
 # TODO: Sketch out the program flow
+# 1. Get the user's name
+# 2. Create a valid sudoku grid composed of a CONTAINER, GRID, and TILES (Tiles are inside GRID)
+# 3. Remove 1 inner 2x2 (Henceforth referred to as GRID) tile to enable sliding
+# 4. Create a valid killer grid (The one that shows the sums for the valid sudoku grid)
+# 5. Randomize the 3 remaining 2x2 tiles position in reference to the outer 2x2 container (Henceforth referred to as CONTAINER)
+# 6. Following a set of rules as to not leave the puzzle unsolvable, remove number from the GRIDs
+# 7. Display the CONTAINER and GRID to the user, overlap the KILLER GRID on top of the CONTAINER
+# 8. Enable the user to slide the GRID tiles
+# 8. Get the user's input
+# 9. Check if the user's CONTAINER matches the originally generated CONTAINER
+# 10. If correct, say YOU WIN, else: do not end the game, the user is able to remove only tiles they have places
 
 
