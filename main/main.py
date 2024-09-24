5859d1418948e4205edcf27b6a5b4b255394d961
ValiValista <a01647566@tec.mx>
2024-09-24 10:36:04 -0600

    Created and commented a starting main file, added instructions on how to use github with references to the documentation.

---
+++ main/main.py
@@ -0,0 +1,14 @@
+# todo = to do list, this shows up in github and IDEs as a todo list
+# How to use github https://docs.github.com/en/get-started/start-your-journey/hello-world
+## 1. Create a new branch
+## 2. Make changes to the branch
+## 3. Commit changes
+## 4. Push changes to the branch
+## 5. Create a pull request
+## 6. Review changes
+## 7. Merge changes
+
+
+# TODO: Sketch out the program flow
+
+
